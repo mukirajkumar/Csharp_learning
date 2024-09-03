71d34e4a4ea5d8b001f4debe467d00d9f0ca274c
mukirajkumar <mukizhitha@gmail.com>
2024-09-03 19:21:45 +0800

    working with numbers and strings

---
+++ Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__learning
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            num++;//increment decrement also can with --
+            Console.WriteLine(5 / 8); //can also use brackets to implement order of operations, also when you do operation with two integers you are gonna get an integer back but if it is btw integer and double, it is gonna return as double
+            Console.WriteLine(7 % 2); //mod operator
+            Console.WriteLine(num);
+            //can use Math methods to do Math operations such as cosine 
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(2, num));
+            Console.WriteLine(Math.Max(0, num)); // also have Min, Round which does rounding to integers
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Muki
+{
+    class strings
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Muki is smart";
+            Console.WriteLine("Muki\nRajkumar");
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.Contains("muki"));
+            Console.WriteLine(phrase.Contains("Muki")); //this method is case sensitive
+            Console.WriteLine(phrase[3]); // indexing
+            Console.WriteLine(phrase.IndexOf("i")); //whether it contains this and at what index which is also case-sensitive
+            Console.WriteLine(phrase.Substring(3,3));
+            Console.ReadLine();
+        }
+    }
+}
