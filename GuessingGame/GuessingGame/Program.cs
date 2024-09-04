6baf2eecab02b1bd26f2cb83e3dd6a1170a45c8c
mukirajkumar <mukizhitha@gmail.com>
2024-09-04 19:26:59 +0800

    created guessing game

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Win32.SafeHandles;
+using System.Transactions;
+
+namespace GuessingGame
+{
+    class GuessingGame
+    {
+        static void Main(string[] args)
+        {
+            string secretWord, guess;
+            bool outOfGuesses = false;
+
+            secretWord = "chocolate";
+            guess = " ";
+            int guessCounter = 0;
+            while (guess != secretWord)
+            {
+                if (guessCounter == 5)
+                {
+                    Console.WriteLine("Sorry, too many guesses! The word you couldn't guess was: " + secretWord);
+                    outOfGuesses = true;
+                    break;
+                }
+                else
+                {
+                    Console.Write("What do you think the word is? Enter a word: ");
+                    guess = Console.ReadLine();
+                    guessCounter++;
+                }
+
+            }
+            if (!outOfGuesses)
+            {
+                Console.WriteLine("You Win!");
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
