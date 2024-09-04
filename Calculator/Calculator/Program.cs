8d90987cb96bab7e85f3d4c69067595de61f0209
mukirajkumar <mukizhitha@gmail.com>
2024-09-04 18:29:16 +0800

    created better calculator and MadLibs

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args) {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Which operation do you want to use? Choose one of the four options:\n1. + \n2. -\n3. *\n4. /\n");
+            int op = Convert.ToInt32(Console.ReadLine());
+            if (op == 1)
+            {
+                Console.Write(num1 + num2);
+            }
+            else if (op == 2)
+            {
+                Console.Write(num1 - num2);
+            }
+            else if (op == 3)
+            {
+                Console.Write(num1 * num2);
+            }
+            else if (op == 4)
+            {
+                Console.Write(num1 / num2);
+            }
+            else {
+                Console.Write("You did not choose correctly");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MadLIbs/MadLIbs/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MadLibs
+{
+    class MadLibs
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I love " + celebrity);
+            Console.ReadLine();
+        }
+    }
+}
