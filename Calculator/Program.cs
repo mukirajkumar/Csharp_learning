5b3f038dca4fc7ee31e83b4495ee4a1c9ed12bca
mukirajkumar <mukizhitha@gmail.com>
2024-09-04 16:46:59 +0800

    building a calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Calculator
+{
+    class Calculator
+    {
+        public Calculator() { }
+        static void Main(string[] args) {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //can also do conversion to double
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+            Console.ReadLine();
+
+        }
+    }
+}
